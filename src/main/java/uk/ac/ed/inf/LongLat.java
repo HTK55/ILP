9924521e8b466a328a5279650b7f67e416c47d3c
HTK55 <s1834153@ed.ac.uk>
2021-09-28 00:13:39 +0100

    LongLat Complete

--- src/main/java/uk/ac/ed/inf/LongLat.java
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -10,19 +10,29 @@ public class LongLat {
     }
 
     public boolean isConfined() {
-        return true;
+        return (this.longitude < -3.184319 && this.longitude > -3.192473 && this.latitude > 55.942617 && this.latitude < 55.946233);
     }
 
     public double distanceTo(LongLat longLat) {
-        return 0;
+        return Math.sqrt(Math.pow((this.longitude - longLat.longitude),2) + Math.pow((this.latitude - longLat.latitude),2));
     }
 
     public boolean closeTo(LongLat longLat) {
-        return true;
+        return (this.distanceTo(longLat) < 0.00015);
     }
 
     public LongLat nextPosition(int angle) {
-        LongLat newLongLat = new LongLat(0.2,0.4);
-        return newLongLat;
+        if (angle == -999) {
+            return this;
+        }
+        else if (angle >= 0 && angle <= 360 && angle % 10 == 0) {
+            double angleInRadians = Math.toRadians(angle);
+            double newLongitude = this.longitude + Math.cos(angleInRadians) * 0.00015;
+            double newLatitude = this.latitude + Math.sin(angleInRadians) * 0.00015;
+            return new LongLat(newLongitude,newLatitude);
+        }
+        else {
+            throw new IllegalArgumentException("This is not a valid angle");
+        }
     }
 }
